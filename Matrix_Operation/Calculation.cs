f4e5df41f29b13cb39cc17c507e84b9adbce3d12
arnab64g <79049651+arnab64g@users.noreply.github.com>
2021-11-10 00:14:28 +0600

    Add files via upload

---
+++ Matrix_Operation/Calculation.cs
@@ -0,0 +1,28 @@
+﻿namespace Matrix_Operation
+{
+    class Matrix_Multiply
+    {
+        public static void multiply(double[,] matA, double[,] matB, double[,] matR, int n)
+        {
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    matR[i, j] = 0;
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    for (int k = 0; k < n; k++)
+                        matR[i, j] += matA[i, k] * matB[k, j];
+        }
+        public static void addition(double[,] matA, double[,] matB, double[,] matR, int n)
+        {
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    matR[i, j] = matA[i, j] + matB[i, j];
+        }
+        public static void substraction(double[,] matA, double[,] matB, double[,] matR, int n)
+        {
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    matR[i, j] = matA[i, j] - matB[i, j];
+        }
+    }
+}
---
+++ Matrix_Operation/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Matrix_Operation
+{
+	partial class Form1
+	{
+		private System.ComponentModel.IContainer components = null;
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+		private void InitializeComponent()
+		{
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.button1 = new System.Windows.Forms.Button();
+			this.cb = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(23, 12);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+			5,
+			0,
+			0,
+			0});
+			this.numericUpDown1.Minimum = new decimal(new int[] {
+			2,
+			0,
+			0,
+			0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(40, 23);
+			this.numericUpDown1.TabIndex = 3;
+			this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			this.numericUpDown1.Value = new decimal(new int[] {
+			2,
+			0,
+			0,
+			0});
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(70, 11);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Next";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// cb
+			// 
+			this.cb.FormattingEnabled = true;
+			this.cb.Items.AddRange(new object[] {
+			"Addition",
+			"Substraction",
+			"Multiplication"});
+			this.cb.Location = new System.Drawing.Point(13, 250);
+			this.cb.Name = "cb";
+			this.cb.Size = new System.Drawing.Size(121, 23);
+			this.cb.TabIndex = 4;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(23, 42);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(52, 15);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Matrix A";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(358, 42);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(51, 15);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Matrix B";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(13, 280);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(0, 15);
+			this.label3.TabIndex = 4;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(173, 12);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "Clear";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(513, 12);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 5;
+			this.button3.Text = "Exit";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(600, 650);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.numericUpDown1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ComboBox cb;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+	}
+}
