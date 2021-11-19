801374bdf956da747e995f1105023c75be9afdc6
arnab64g <79049651+arnab64g@users.noreply.github.com>
2021-11-19 20:50:34 +0600

    Add files via upload

---
+++ A_MP3_Player/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace A_Music_Player
+
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MusicList = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MusicList
+            // 
+            this.MusicList.AllowDrop = true;
+            this.MusicList.BackColor = System.Drawing.SystemColors.Info;
+            this.MusicList.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.MusicList.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MusicList.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.MusicList.FormattingEnabled = true;
+            this.MusicList.HorizontalScrollbar = true;
+            this.MusicList.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.MusicList.ItemHeight = 16;
+            this.MusicList.Location = new System.Drawing.Point(341, 50);
+            this.MusicList.Margin = new System.Windows.Forms.Padding(5);
+            this.MusicList.Name = "MusicList";
+            this.MusicList.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.MusicList.Size = new System.Drawing.Size(320, 292);
+            this.MusicList.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Location = new System.Drawing.Point(339, 350);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(337, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Music List";
+            this.label1.UseWaitCursor = true;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(288, 321);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 104);
+            this.trackBar1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(12, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 18);
+            this.label2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label3.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 21);
+            this.label3.TabIndex = 10;
+            this.label3.Text = " ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(12, 286);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(321, 45);
+            this.trackBar2.TabIndex = 11;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.checkBox1.Location = new System.Drawing.Point(12, 372);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(67, 18);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "Repeat";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.button6.Image = global::WindowsFormsApp1.Properties.Resources.icons8_fast_forward_48;
+            this.button6.Location = new System.Drawing.Point(222, 360);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(36, 30);
+            this.button6.TabIndex = 15;
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.Image = global::WindowsFormsApp1.Properties.Resources.icons8_rewind_48;
+            this.button5.Location = new System.Drawing.Point(102, 361);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(33, 29);
+            this.button5.TabIndex = 14;
+            this.button5.Text = " ";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Image = global::WindowsFormsApp1.Properties.Resources.icons8_stop_48;
+            this.button4.Location = new System.Drawing.Point(156, 407);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(46, 31);
+            this.button4.TabIndex = 9;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Image = global::WindowsFormsApp1.Properties.Resources.icons8_pause_48;
+            this.button3.Location = new System.Drawing.Point(141, 337);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 64);
+            this.button3.TabIndex = 8;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Leelawadee", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Image = global::WindowsFormsApp1.Properties.Resources.icons8_play_48;
+            this.button2.Location = new System.Drawing.Point(141, 338);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 64);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("PMingLiU-ExtB", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(285, 428);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 11);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Volume";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.avatars_000606604806_j6ghpm_t500x500;
+            this.pictureBox1.Location = new System.Drawing.Point(117, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(216, 230);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label5.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 259);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 21);
+            this.label5.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.ClientSize = new System.Drawing.Size(680, 457);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MusicList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "A Music Player";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+        private System.Windows.Forms.ListBox MusicList;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar2;
+        public System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ A_MP3_Player/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+using WMPLib;
+namespace A_Music_Player
+{
+    public partial class Form1 : Form
+    {
+        double pos = 0;
+        bool run = false;
+        int count = 0, ci = 0;   
+        WindowsMediaPlayer wpl = new WindowsMediaPlayer();
+        string[] s = new string[100];
+        details[] d = new details[100];
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += Browse_Music;
+            button2.Click += Play_Music;
+            trackBar1.Value = 67;
+            trackBar2.Value = 0;
+            wpl.settings.volume = trackBar1.Value;
+            trackBar1.Scroll += TrackBar1_Scroll;
+            trackBar2.Scroll += Fo_Music;
+            button3.Click += Push_Music;
+            MusicList.MouseClick += Play_Music_s;
+            button4.Click += Stop_Current;
+            button6.Click += Play_Next;
+            button5.Click += Play_Prev;
+        }
+        private void Play_Prev(object sender, EventArgs e)
+        {
+            ci--;
+            wpl.controls.stop();
+            if (ci < 0)
+            {
+                ci = count - 1;
+                if (checkBox1.Checked == false)
+                    button4.PerformClick();
+                else{
+                    MusicList.SelectedIndex = ci;
+                    button2.PerformClick();
+                }
+            }
+            else{
+                MusicList.SelectedIndex = ci;
+                button2.PerformClick();
+            }
+        }
+        private void Play_Next(object sender, EventArgs e)
+        {
+            wpl.controls.stop();
+            if (ci == count){
+                ci = 0;
+                if (checkBox1.Checked == false)
+                    button4.PerformClick();
+                else{
+                    MusicList.SelectedIndex = ci;
+                    button2.PerformClick();
+                }
+            }
+            else{
+                MusicList.SelectedIndex = ci;
+                button2.PerformClick();
+            }
+        }
+        private void Play_Music_s(object sender, MouseEventArgs e)
+        {
+            run = false;
+            Console.WriteLine("Clicked");
+            wpl.controls.stop();
+            button2.PerformClick();
+        }
+        private void Push_Music(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            pos = wpl.controls.currentPosition;
+            wpl.controls.pause();
+            button2.Visible = true;
+            button3.Visible = false;
+        }
+        private void Stop_Current(object sender, EventArgs e)
+        {
+            run = false;
+            pos = 0;
+            wpl.controls.stop();
+            button2.Text = "Play";
+            timer1.Stop();
+            button2.Visible = true;
+            button3.Visible = false;
+            button4.Visible = false;
+        }
+        private void Fo_Music(object sender, EventArgs e)
+        {
+            wpl.controls.currentPosition = (double) (trackBar2.Value * wpl.controls.currentItem.duration)/100;
+        }
+        private void TrackBar1_Scroll(object sender, EventArgs e)
+        {
+            wpl.settings.volume = trackBar1.Value;
+        }
+        public void Browse_Music(Object sender, EventArgs e)
+        {   
+            OpenFileDialog opf = new OpenFileDialog();
+            opf.Title = "A Music Player";
+            opf.Multiselect = true;
+            opf.ShowDialog();
+            foreach (var item in opf.FileNames){
+                s[count++] = item;
+            }
+            Song_list.list_make(s, d, count);
+            for (int i = 0; i < count; i++)
+                MusicList.Items.Add(d[i].name);
+        }
+        public void Play_Music(Object sender, EventArgs e)
+        {
+            string s;
+            if (MusicList.SelectedIndex == -1){
+                s = MusicList.Items[0].ToString();
+                ci = 0;
+            }
+            else{
+                s = MusicList.SelectedItem.ToString();
+                ci = MusicList.SelectedIndex;
+            }
+            if (!run)
+                wpl.URL = Song_list.getPath(s, d, count);
+            wpl.controls.play();
+            try
+            {
+                var taglb = TagLib.File.Create(wpl.URL);
+                var bin = (byte[])(taglb.Tag.Pictures[0].Data.Data);
+                pictureBox1.Image = Image.FromStream(new MemoryStream(bin));
+            }
+            catch
+            { 
+            }
+            wpl.controls.currentPosition = pos;
+            
+            timer1.Start();
+            timer1.Interval = 100;
+            timer1.Tick += Update_bar;
+            label2.Text = wpl.status;   
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Update_bar(object sender, EventArgs e)
+        {
+            label2.Text = "Now Playing: " + wpl.controls.currentItem.name;
+            if (wpl.controls.currentItem.duration > 0){
+                var t = TimeSpan.FromSeconds(wpl.controls.currentPosition);
+                label3.Text = t.ToString(@"mm\:ss");
+                label5.Text = wpl.controls.currentItem.durationString.ToString();
+                trackBar2.Value = (int)((wpl.controls.currentPosition * 100) / wpl.controls.currentItem.duration);
+            }
+            Console.WriteLine(wpl.playState.ToString());
+            if(wpl.playState == WMPPlayState.wmppsPlaying){
+                button2.Visible = false;
+                button3.Visible = true;
+                button4.Visible = true;
+            }
+            else if (wpl.playState != WMPPlayState.wmppsPlaying){
+                button2.Visible = true;
+                button3.Visible = false;
+                button4.Visible = false;
+            }
+            if(wpl.playState == WMPPlayState.wmppsStopped){
+                ci++;
+                button6.PerformClick();
+            } 
+        }
+    }
+}
